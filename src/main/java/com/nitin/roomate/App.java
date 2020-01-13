c012b3db06ad28982846bd9ab0cc98403e7d9216
nitinrane <nitinanantarane@gmail.com>
2020-01-13 11:45:33 -0500

    First commit

---
+++ src/main/java/com/nitin/roomate/App.java
@@ -0,0 +1,13 @@
+package com.nitin.roomate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
