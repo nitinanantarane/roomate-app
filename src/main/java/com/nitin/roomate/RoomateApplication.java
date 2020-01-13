92355f11b84e77fcebe3db1d44683b53a39d142c
nitinrane <nitinanantarane@gmail.com>
2020-01-13 15:01:14 -0500

    Springboot sample

---
+++ src/main/java/com/nitin/roomate/RoomateApplication.java
@@ -0,0 +1,12 @@
+package com.nitin.roomate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomateApplication.class, args);
+	}
+}
